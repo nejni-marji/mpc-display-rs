cda27051da066101b255d174f517fa32bc5fe629
nejni-marji <nejni.marji@gmail.com>
2024-06-07 17:20:50 -0500

    create timeout loop for connecting to the client

--- src/player.rs
+++ src/player.rs
@@ -4,39 +4,56 @@ mod input;
 use display::Display;
 use input::KeyHandler;
 
-use std::thread;
 use std::io;
 use std::io::Write;
+use std::thread;
+use std::time::Duration;
 
 use mpd::Client;
 use uuid::Uuid;
 
 pub struct Player;
 
+const TIMEOUT_COUNT: i32 = 10;
+const TIMEOUT_DELAY: u64 = 1;
+
 impl Player {
     pub fn init(address: String, format: Vec<String>) {
         // generate UUID for proper quit handling
         let uuid = Uuid::new_v4();
 
-        // attempt to connect clients. all failure happens within this function.
-        Self::connect(address, format, uuid);
+        // loop around connect()
+        for i in 0..TIMEOUT_COUNT {
+            let result = Self::connect(
+                address.clone(), format.clone(), uuid.clone());
+            if let Ok(_) = result {
+                // reset terminal before exit
+                print!("\x1b[?25h\x1b[2J");
+                io::stdout().flush().expect("unable to flush buffer");
 
-        // reset terminal before exit
-        print!("\x1b[?25h\x1b[2J");
-        io::stdout().flush().expect("unable to flush buffer");
+                // exit loop
+                break
+            }
+            println!("connection failed {i}/{TIMEOUT_COUNT}");
+            thread::sleep(Duration::from_secs(TIMEOUT_DELAY));
+        }
     }
 
-    fn connect(address: String, format: Vec<String>, uuid: Uuid) {
+    // all failure gets passed up to this function
+    fn connect(address: String, format: Vec<String>, uuid: Uuid) -> Result<(), mpd::error::Error> {
         // run player
-        let player_client = Client::connect(address.clone())
-            .unwrap();
+        let player_client = Client::connect(&address)?;
         let mut player = Display::new(player_client, format, uuid);
-        thread::spawn(move || { player.init(); });
+        let thr = thread::spawn(move || { player.init(); });
 
         // initialize input
-        let parser_client = Client::connect(address)
-            .unwrap();
+        let parser_client = Client::connect(address)?;
         let mut parser = KeyHandler::new(parser_client, uuid);
-        parser.init();
+        let par = parser.init();
+
+        // join display thread before exit
+        let dsp = thr.join().unwrap();
+
+        Ok(())
     }
 }
