65f0c2aced43427ae36822dfee0e57c7b73defe4
nejni-marji <nejni.marji@gmail.com>
2024-06-13 14:52:09 -0500

    Revert "only use debug_print on debug"
    This reverts commit b1bdc303db8e1688f5e37d720394e71da4816fdf.

--- src/player/display.rs
+++ src/player/display.rs
@@ -11,7 +11,6 @@ use terminal_size::terminal_size;
 use uuid::Uuid;
 
 #[allow(unused_imports)]
-#[cfg(debug_assertions)]
 use debug_print::{
     debug_print as dprint,
     debug_println as dprintln,
