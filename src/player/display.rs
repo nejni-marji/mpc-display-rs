fe17ecc4b7f71ffb678a2fcfa1c16c545d20a76a
nejni-marji <nejni.marji@gmail.com>
2024-06-18 16:28:41 -0500

    add todo

--- src/player/display.rs
+++ src/player/display.rs
@@ -39,6 +39,7 @@ pub struct Display {
 #[derive(Debug,Default,Clone)]
 struct MusicData {
     // non-music data
+    // TODO: move these to a new type
     format: Vec<String>,
     verbose: bool,
     verbose_tags: Vec<bool>,
