906d880a53479abb98103805f3518bc6add69851
nejni-marji <nejni.marji@gmail.com>
2025-03-01 17:27:03 -0600

    handle cannot connect to server at startup

--- src/player.rs
+++ src/player.rs
@@ -20,20 +20,26 @@ impl Player {
         let uuid = Uuid::new_v4();
 
         // initialize display
-        let display_client = Client::connect(&address)
-            .expect("can't connect to client");
+        let Ok(display_client) = Client::connect(&address) else {
+            return Self::error(&address)
+        };
         let mut display = Display::new(display_client, format,
             uuid, options);
         let display = thread::spawn(move || { display.init() });
 
         // initialize input
-        let input_client = Client::connect(address)
-            .expect("can't connect to client");
+        let Ok(input_client) = Client::connect(&address) else {
+            return Self::error(&address)
+        };
         let input = KeyHandler::new(input_client, uuid);
         let input = thread::spawn(move || { input.init() });
 
         // join threads and check for panics
         let _ = display.join();
         let _ = input.join();
+    }
+
+    fn error(address: &str) {
+        println!("Cannot connect to server: {address}");
     }
 }
